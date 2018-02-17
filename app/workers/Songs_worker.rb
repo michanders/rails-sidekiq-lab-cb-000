e30650be75464f134ab59f1c1054658f9e647937
Michael Anderson <michanders@msn.com>
2018-02-17 22:58:22 +0000

    automatically backed up by learn

--- app/workers/Songs_worker.rb
+++ app/workers/Songs_worker.rb
@@ -3,7 +3,7 @@ class SongsWorker
   include Sidekiq::Worker
 
   def perform(leads_file)
-    CSV.foreach(params["file"].path, headers: true) do |song|
+    CSV.foreach(params[leads_file].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
     end
   end
